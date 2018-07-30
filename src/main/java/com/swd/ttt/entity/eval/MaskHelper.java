41e3325fb96e729fb874566988b3670ac828ff70
Scott Davidson <scottdavidson@Scotts-MacBook-Pro.local>
2018-07-30 15:55:04 -0700

    Implemented SingleColumnHeuristic, SingleRowColumnDiagonalEval and unit test

--- src/main/java/com/swd/ttt/entity/eval/MaskHelper.java
+++ src/main/java/com/swd/ttt/entity/eval/MaskHelper.java
@@ -9,6 +9,7 @@ public class MaskHelper {
      * Returns <tt>true</tt> if the provided mask applied to the player representation leads to the provided
      * expected mask representation.
      */
+    // TODO should probably move this to be available by Heuristics too ( since eval is dependent on heuristic anyway
     public static boolean mask(short playerRepresentation, short mask, short expectedMaskedPlayerRepresentation){
         int maskedPlayerRepresentation = playerRepresentation & mask;
         return (short) maskedPlayerRepresentation == expectedMaskedPlayerRepresentation;
