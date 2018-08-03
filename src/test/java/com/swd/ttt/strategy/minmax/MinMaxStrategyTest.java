0d8a031620f022ad09da4bf18a878160a2cc468b
Scott Davidson <scottdavidson@Scotts-MacBook-Pro.local>
2018-08-03 10:07:03 -0700

    Added more moves

--- src/test/java/com/swd/ttt/strategy/minmax/MinMaxStrategyTest.java
+++ src/test/java/com/swd/ttt/strategy/minmax/MinMaxStrategyTest.java
@@ -36,16 +36,35 @@ public class MinMaxStrategyTest {
     public static Object[][] testExecuteMoveData() {
 
         // Construct of series of moves
+        //
+        // The focus for the test is to almost fill TTT board #4 and have the next play there
+        // to test the executeMove(...) method; here's what board #4 looks like:
+        //
+        //         | X | O
+        //       ---------
+        //       O | X | X
+        //       ---------
+        //         | O | X
         List<MovePosition> movePositions = new ArrayList<MovePosition>();
-        movePositions.add(MovePosition.newMovePosition(4,4));
-        movePositions.add(MovePosition.newMovePosition(4,2));
+        movePositions.add(MovePosition.newMovePosition(4,4));  // X
+        movePositions.add(MovePosition.newMovePosition(4,2));  // O
         movePositions.add(MovePosition.newMovePosition(2,0));
         movePositions.add(MovePosition.newMovePosition(0,6));
         movePositions.add(MovePosition.newMovePosition(6,1));
         movePositions.add(MovePosition.newMovePosition(1,4));
-        movePositions.add(MovePosition.newMovePosition(4,5));
+        movePositions.add(MovePosition.newMovePosition(4,5));  // X
         movePositions.add(MovePosition.newMovePosition(5,4));
-        movePositions.add(MovePosition.newMovePosition(4,3));
+        movePositions.add(MovePosition.newMovePosition(4,1));  // X
+        movePositions.add(MovePosition.newMovePosition(1,7));
+        movePositions.add(MovePosition.newMovePosition(7,4));
+        movePositions.add(MovePosition.newMovePosition(4,3));  // O
+        movePositions.add(MovePosition.newMovePosition(3,4));
+        movePositions.add(MovePosition.newMovePosition(4,7));  // O
+        movePositions.add(MovePosition.newMovePosition(7,6));
+        movePositions.add(MovePosition.newMovePosition(6,4));
+        movePositions.add(MovePosition.newMovePosition(4,8));  // X
+        movePositions.add(MovePosition.newMovePosition(8,4));  // X
+
 
         // Instantiate a Board and execute a series of moves
         Board board = executeMultipleTurns(Board.initialBoard(1000, Player.X, 4),
